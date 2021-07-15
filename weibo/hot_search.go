071bb48fa745a6f924bd4e24244335b062c6cbda
akez <1791189210@qq.com>
2021-07-15 15:47:36 +0800

    抓取内容写入csv

--- weibo/hot_search.go
+++ weibo/hot_search.go
@@ -1,19 +1,41 @@
 package weibo
 
 import (
+	"encoding/csv"
 	"github.com/PuerkitoBio/goquery"
 	"github.com/gocolly/colly"
 	"log"
+	"os"
 	"strconv"
 )
 
-func HotSearchDetails(c *colly.Collector, link string, rank int64, content string, hot int64) {
+func WriteCSV(str []string) {
+	file, err := os.OpenFile("hot-search.csv", os.O_RDWR|os.O_APPEND|os.O_CREATE, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(file)
+
+	writer := csv.NewWriter(file)
+	err = writer.Write(str)
+	if err != nil {
+		log.Fatal(err)
+	}
+	writer.Flush()
+}
+
+func HotSearchDetails(c *colly.Collector, link string, rank string, content string, hot string) {
 	c = c.Clone()
 	// 获取微博热搜详情,得到导语
 	c.OnHTML("div#pl_feedlist_index", func(e *colly.HTMLElement) {
 		topicLead := e.DOM.Find("div.card-wrap >div.card.card-topic-lead.s-pg16 >p").Text()
-		log.Printf("rank: %v, content: %v\n, link: %v\n, hot: %v\n, topic-lead: %v\n",
-			rank, content, link, hot, topicLead)
+		str := []string{rank, link, content, hot, topicLead}
+		WriteCSV(str)
 	})
 
 	err := c.Visit(link)
@@ -36,16 +58,15 @@ func HotSearch() {
 				// 不是真正热搜内容
 			}
 			// 热搜排名
-			rankStr := selection.Find("td.td-01.ranktop").Text()
+			rank := selection.Find("td.td-01.ranktop").Text()
 			// 判断排名是否为数字,热搜中可能穿插其他内容
-			rank, err := strconv.ParseInt(rankStr, 10, 64)
+			_, err := strconv.ParseInt(rank, 10, 64)
 			// 真正热搜内容
 			if err == nil {
 				// 热搜内容
 				content := selection.Find("td.td-02 >a").Text()
 				// 热度
-				hotStr := selection.Find("td.td-02 >span").Text()
-				hot, _ := strconv.ParseInt(hotStr, 10, 64)
+				hot := selection.Find("td.td-02 >span").Text()
 				// 热搜链接
 				link, exists := selection.Find("td.td-02 >a").Attr("href")
 				if exists {
