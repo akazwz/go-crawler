b968bccccf14ca4712401d4c38f5ffc9236ed54f
akez <1791189210@qq.com>
2021-07-15 15:23:52 +0800

    微博热搜增加详情页面导语获取

--- weibo/hot_search.go
+++ weibo/hot_search.go
@@ -7,6 +7,21 @@ import (
 	"strconv"
 )
 
+func HotSearchDetails(c *colly.Collector, link string, rank int64, content string, hot int64) {
+	c = c.Clone()
+	// 获取微博热搜详情,得到导语
+	c.OnHTML("div#pl_feedlist_index", func(e *colly.HTMLElement) {
+		topicLead := e.DOM.Find("div.card-wrap >div.card.card-topic-lead.s-pg16 >p").Text()
+		log.Printf("rank: %v, content: %v\n, link: %v\n, hot: %v\n, topic-lead: %v\n",
+			rank, content, link, hot, topicLead)
+	})
+
+	err := c.Visit(link)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func HotSearch() {
 	c := colly.NewCollector()
 	url := "https://s.weibo.com/top/summary/"
@@ -21,19 +36,21 @@ func HotSearch() {
 				// 不是真正热搜内容
 			}
 			// 热搜排名
-			rank := selection.Find("td.td-01.ranktop").Text()
-			// 判断排名是否为数子,热搜中可能穿插其他内容
-			_, err := strconv.ParseFloat(rank, 64)
+			rankStr := selection.Find("td.td-01.ranktop").Text()
+			// 判断排名是否为数字,热搜中可能穿插其他内容
+			rank, err := strconv.ParseInt(rankStr, 10, 64)
 			// 真正热搜内容
 			if err == nil {
 				// 热搜内容
 				content := selection.Find("td.td-02 >a").Text()
 				// 热度
-				hot := selection.Find("td.td-02 >span").Text()
+				hotStr := selection.Find("td.td-02 >span").Text()
+				hot, _ := strconv.ParseInt(hotStr, 10, 64)
 				// 热搜链接
-				link, _ := selection.Find("td.td-02 >a").Attr("href")
-				log.Printf("rank: %v, content: %v, link: %v, hot: %v",
-					rank, content, link, hot)
+				link, exists := selection.Find("td.td-02 >a").Attr("href")
+				if exists {
+					HotSearchDetails(c, "https://s.weibo.com"+link, rank, content, hot)
+				}
 			}
 		})
 	})
